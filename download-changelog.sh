836fb5378dd6bddae56efcdb5699692009beddfd
tianyong90 <412039588@qq.com>
2017-09-14 14:01:15 +0800

    changelog

--- download-changelog.sh
+++ download-changelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -nv --no-check-certificate https://raw.githubusercontent.com/tianyong90/we-vue/master/CHANGELOG.md -O ./src/assets/docs/changelog.md
-git add .
-git commit -m 'download changelog.md'
-git push origin master
+wget -nv --no-check-certificate https://raw.githubusercontent.com/tianyong90/we-vue/master/CHANGELOG.md -O '/mnt/d/projects/we-vue-docs-site/src/docs/changelog.md'
+# git add .
+# git commit -m 'download changelog.md'
+# git push origin master
